7221be5ddbcba332997f8f56dbc451a0c67bb689
Erfan Noury <erfan.noury@live.com>
2018-04-24 15:59:06 -0400

    Change the order configs are overwritter in test_net.py

--- tools/test_net.py
+++ tools/test_net.py
@@ -67,10 +67,10 @@ if __name__ == '__main__':
     print('Called with args:')
     print(args)
 
-    if args.cfg_file is not None:
-        cfg_from_file(args.cfg_file)
     if args.set_cfgs is not None:
         cfg_from_list(args.set_cfgs)
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
 
     print('Using config:')
     pprint.pprint(cfg)
